f063aa4155fc3026625b75fbf3a13c51b30ddbed
tpargal35 <tpargal35@gmail.com>
2022-05-15 20:03:09 +0530

    Added js and left right movement for race car and random appearence of blue car

---
+++ main.js
@@ -0,0 +1,16 @@
+var bluecar = document.getElementById("bluecar");
+var racecar = document.getElementById("racecar");
+
+// Blue car movement
+bluecar.addEventListener("animationiteration", function(){
+    var random = ( (Math.floor(Math.random() * 3 )) * 130);
+    bluecar.style.left = random + "px";
+})
+
+//Race car movement
+window.addEventListener("keydown", function(e){
+    if (e.keyCode == 39) { var racecarLeft = parseInt(window.getComputedStyle(racecar).getPropertyValue("left"));
+    if(racecarLeft<260){ racecar.style.left = (racecarLeft+130)+"px"}}
+    if (e.keyCode == 37){var racecarLeft = parseInt(window.getComputedStyle(racecar).getPropertyValue("left"));
+    if(racecarLeft>0){ racecar.style.left = (racecarLeft-130)+"px"}}
+})
