4a13429431574047419923050f2b3ee849fc70a5
tpargal35 <tpargal35@gmail.com>
2022-05-15 20:24:50 +0530

    Final Commit

--- main.js
+++ main.js
@@ -1,16 +1,48 @@
-var bluecar = document.getElementById("bluecar");
-var racecar = document.getElementById("racecar");
+var blueCar = document.getElementById("bluecar");
+var raceCar = document.getElementById("racecar");
+var result = document.getElementById("result")
+const score =  document.getElementById("score")
+var game =  document.getElementById("game");
+var counter = 0;
+var jumpsound = document.getElementById("jumpsound")
 
-// Blue car movement
-bluecar.addEventListener("animationiteration", function(){
-    var random = ( (Math.floor(Math.random() * 3 )) * 130);
-    bluecar.style.left = random + "px";
+
+
+// bluecar move
+blueCar.addEventListener("animationiteration", function(){
+    var random = ((Math.floor(Math.random() * 3)) * 130)
+    blueCar.style.left = random + "px";
+    counter++
 })
 
-//Race car movement
+//rececar move
 window.addEventListener("keydown", function(e){
-    if (e.keyCode == 39) { var racecarLeft = parseInt(window.getComputedStyle(racecar).getPropertyValue("left"));
-    if(racecarLeft<260){ racecar.style.left = (racecarLeft+130)+"px"}}
-    if (e.keyCode == 37){var racecarLeft = parseInt(window.getComputedStyle(racecar).getPropertyValue("left"));
-    if(racecarLeft>0){ racecar.style.left = (racecarLeft-130)+"px"}}
+   if(e.keyCode == "39"){ var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"))
+    if(raceCarLeft < 260){raceCar.style.left = (raceCarLeft + 130) + "px"}
+    jumpsound.play()
+};
+
+    if(e.keyCode == "37"){
+        var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"))
+        if(raceCarLeft > 0){raceCar.style.left = (raceCarLeft - 130) + "px"
+        jumpsound.play()
+    }
+
+    }
 })
+
+
+//Game over
+setInterval(function Gameover (){
+    var blueCarTop = parseInt(window.getComputedStyle(blueCar).getPropertyValue("top"))
+    var blueCarLeft = parseInt(window.getComputedStyle(blueCar).getPropertyValue("left"));
+    var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
+        if((blueCarLeft === raceCarLeft) && (blueCarTop > 250) && (blueCarTop < 450)){
+            result.style.display = "block";
+            game.style.display = "none";
+            score.innerHTML = `score: ${counter} `;
+
+            counter = 0;
+        }
+}, 10)
+
